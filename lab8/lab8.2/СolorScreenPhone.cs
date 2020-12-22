029d2e3e97b898fb0b2331d2889dcb60bd58eeb1
denchikkiller3 <59163776+denchikkiller3@users.noreply.github.com>
2020-12-22 23:17:17 +0200

    Create СolorScreenPhone.cs

---
+++ lab8/lab8.2/СolorScreenPhone.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8.2
+{
+    public class СolorScreenPhone : BlackWhitePhone
+    {
+        public int CountOfColors { get; set; }
+        public bool SupportDualSim { get; set; }
+        public string SecondNumber { get; set; }
+    }
+}
